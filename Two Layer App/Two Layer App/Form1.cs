fcdf21fb5032efd56acd0ed675a895d25b99ff01
Simon Pangan <simonjoseph.pangan@benilde.edu.ph>
2021-09-17 01:14:58 +0800

    Intepro 2 layer App

---
+++ Two Layer App/Two Layer App/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Intepro.DataAccess;   //Access to DAL
+
+namespace Two_Layer_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private DAL dl = new DAL();
+        private void button1_Click(object sender, EventArgs e)
+        {   //Add button
+            dl.Open();
+            dl.SetSql("INSERT INTO Participants " +
+                " VALUES (@id, @ln, @fn)");
+            dl.AddParam("@id", txtID.Text);
+            dl.AddParam("@ln", txtLN.Text);
+            dl.AddParam("@fn", txtFN.Text);
+            dl.Execute();
+            dl.Close();
+
+            //MessageBox.Show("Add successful!");
+            //txtID.Clear(); txtLN.Clear(); txtFN.Clear();
+            //button4_Click(sender, e);
+            Complete("Add Successful!", sender, e);
+        }
+        private void Complete(string message,
+            object sender, EventArgs e)
+        {
+            MessageBox.Show(message);
+            txtID.Clear(); txtLN.Clear(); txtFN.Clear();
+            button4_Click(sender, e);
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {   //View all button
+            dl.Open();
+            dl.SetSql("SELECT * FROM Participants");
+            dgvP.DataSource = dl.GetRecords();
+            dl.Close();
+        }
+
+        private void dgvP_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtID.Text = dgvP.SelectedRows[0].Cells[0].Value.ToString();
+            txtLN.Text = dgvP.SelectedRows[0].Cells[1].Value.ToString();
+            txtFN.Text = dgvP.SelectedRows[0].Cells[2].Value.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button4_Click(sender, e);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {   //Edit button
+            dl.Open();
+            dl.SetSql("UPDATE Participants SET Lastname = @ln, " +
+                "Firstname = @fn WHERE ID = @id");
+            dl.AddParam("@ln", txtLN.Text);
+            dl.AddParam("@fn", txtFN.Text);
+            dl.AddParam("@id", txtID.Text);
+            dl.Execute();
+            dl.Close();
+
+            Complete("Update succesful!", sender, e);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            dl.Open();
+            dl.SetSql("DELETE Participants WHERE ID = @id");
+            dl.AddParam("@id",
+                dgvP.SelectedRows[0].Cells[0].Value.ToString());
+            dl.Execute();
+            dl.Close();
+            Complete("Delete successful!", sender, e);
+        }
+    }
+}
